6ec98d4c80a8f5eba49d8647f5502cec57795864
Kolchin Vladimir <Kolchin_Vladimir@mail.ru>
2024-12-23 09:34:33 +0300

    change README and add -d to start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #path=$(cat .env | grep DATA | awk -F= '{print $2}')
-docker run --name ftp-server --rm --env-file=.env -v ftp_volume:/data -p 20-21:20-21 -p 20000-20010:20000-20010 ftp:latest
+docker run --name ftp-server --rm -d --env-file=.env -v ftp_volume:/data -p 20-21:20-21 -p 20000-20010:20000-20010 ftp:latest
